db3cdf99ddc0c95a2c2c933f563e96951081e8a2
Arukim <smelovn@gmail.com>
2016-05-30 23:15:58 +0300

    added hosting.json

--- Terra.WebUI/Program.cs
+++ Terra.WebUI/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace Terra.WebUI
 {
@@ -11,7 +12,13 @@ namespace Terra.WebUI
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("hosting.json", optional: false)
+                .Build();
+            
             var host = new WebHostBuilder()
+                .UseConfiguration(config)
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
